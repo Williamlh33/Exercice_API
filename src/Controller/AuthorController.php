c08a73e307225c9cc6078d034cbca5c1cc45d461
William-lh <lherm.william@gmail.com>
2024-07-21 13:53:07 +0200

    first commit

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use JMS\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AuthorController extends AbstractController
+{
+    
+    /**
+     * Cette méthode permet de récupérer l'ensemble des auteurs. 
+     *
+     * @param AuthorRepository $authorRepository
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    #[Route('/api/authors', name: 'authors', methods: ['GET'])]
+    public function getAllAuthors(AuthorRepository $authorRepository, SerializerInterface $serializer, 
+        Request $request, TagAwareCacheInterface $cache): JsonResponse
+    {
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+        
+        $idCache = "getAllAuthor-" . $page . "-" . $limit;
+
+        $jsonAuthorList = $cache->get($idCache, function (ItemInterface $item) use ($authorRepository, $page, $limit, $serializer) {
+            //echo ("L'ELEMENT N'EST PAS ENCORE EN CACHE !\n");
+            $item->tag("booksCache");
+            $authorList = $authorRepository->findAllWithPagination($page, $limit);
+            $context = SerializationContext::create()->setGroups(["getAuthors"]);
+            return $serializer->serialize($authorList, 'json', $context);
+        });
+        
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+	
+    /**
+     * Cette méthode permet de récupérer un auteur en particulier en fonction de son id. 
+     *
+     * @param Author $author
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    #[Route('/api/authors/{id}', name: 'detailAuthor', methods: ['GET'])]
+    public function getDetailAuthor(Author $author, SerializerInterface $serializer): JsonResponse {
+        $context = SerializationContext::create()->setGroups(["getAuthors"]);
+        $jsonAuthor = $serializer->serialize($author, 'json', $context);
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
+    }
+
+    
+    /**
+     * Cette méthode supprime un auteur en fonction de son id. 
+     * En cascade, les livres associés aux auteurs seront aux aussi supprimés. 
+     *
+     * /!\ Attention /!\
+     * pour éviter le problème :
+     * "1451 Cannot delete or update a parent row: a foreign key constraint fails"
+     * Il faut bien penser rajouter dans l'entité Book, au niveau de l'author :
+     * #[ORM\JoinColumn(onDelete:"CASCADE")]
+     * 
+     * Et resynchronizer la base de données pour appliquer ces modifications. 
+     * avec : php bin/console doctrine:schema:update --force
+     * 
+     * @param Author $author
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    #[Route('/api/authors/{id}', name: 'deleteAuthor', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour supprimer un auteur')]
+    public function deleteAuthor(Author $author, EntityManagerInterface $em, TagAwareCacheInterface $cache): JsonResponse {
+        
+        $em->remove($author);
+        $em->flush();
+
+        // On vide le cache.
+        $cache->invalidateTags(["booksCache"]);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Cette méthode permet de créer un nouvel auteur. Elle ne permet pas 
+     * d'associer directement des livres à cet auteur. 
+     * Exemple de données :
+     * {
+     *     "lastName": "Tolkien",
+     *     "firstName": "J.R.R"
+     * }
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @param UrlGeneratorInterface $urlGenerator
+     * @return JsonResponse
+     */
+    #[Route('/api/authors', name: 'createAuthor', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour créer un auteur')]
+    public function createAuthor(Request $request, SerializerInterface $serializer,
+        EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator,
+        TagAwareCacheInterface $cache): JsonResponse {
+        $author = $serializer->deserialize($request->getContent(), Author::class, 'json');
+        
+        // On vérifie les erreurs
+        $errors = $validator->validate($author);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+        
+        $em->persist($author);
+        $em->flush();
+        
+        // On vide le cache. 
+        $cache->invalidateTags(["booksCache"]);
+
+        $context = SerializationContext::create()->setGroups(["getAuthors"]);
+        $jsonAuthor = $serializer->serialize($author, 'json', $context);
+        $location = $urlGenerator->generate('detailAuthor', ['id' => $author->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new JsonResponse($jsonAuthor, Response::HTTP_CREATED, ["Location" => $location], true);	
+    }
+
+    
+    /**
+     * Cette méthode permet de mettre à jour un auteur. 
+     * Exemple de données :
+     * {
+     *     "lastName": "Tolkien",
+     *     "firstName": "J.R.R"
+     * }
+     * 
+     * Cette méthode ne permet pas d'associer des livres et des auteurs.
+     * 
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param Author $currentAuthor
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    #[Route('/api/authors/{id}', name:"updateAuthor", methods:['PUT'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour éditer un auteur')]
+    public function updateAuthor(Request $request, SerializerInterface $serializer,
+        Author $currentAuthor, EntityManagerInterface $em, ValidatorInterface $validator,
+        TagAwareCacheInterface $cache): JsonResponse {
+
+        // On vérifie les erreurs
+        $errors = $validator->validate($currentAuthor);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $newAuthor = $serializer->deserialize($request->getContent(), Author::class, 'json');
+        $currentAuthor->setFirstName($newAuthor->getFirstName());
+        $currentAuthor->setLastName($newAuthor->getLastName());
+        
+        $em->persist($currentAuthor);
+        $em->flush();
+
+        // On vide le cache. 
+        $cache->invalidateTags(["booksCache"]);
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+
+    }
+}
---
+++ src/Controller/BookController.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use App\Repository\AuthorRepository;
+use JMS\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\VersioningService;
+
+class BookController extends AbstractController
+{   
+    /**
+     * Cette méthode permet de récupérer l'ensemble des livres. 
+     *
+     * @param BookRepository $bookRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @return JsonResponse
+     */
+    #[Route('/api/books', name: 'books', methods: ['GET'])]
+    public function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer, Request $request, TagAwareCacheInterface $cache): JsonResponse
+    {
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+
+        $idCache = "getAllBooks-" . $page . "-" . $limit;
+        
+        $jsonBookList = $cache->get($idCache, function (ItemInterface $item) use ($bookRepository, $page, $limit, $serializer) {
+            //echo ("L'ELEMENT N'EST PAS ENCORE EN CACHE !\n");
+            $item->tag("booksCache");
+            $bookList = $bookRepository->findAllWithPagination($page, $limit);
+            $context = SerializationContext::create()->setGroups(["getBooks"]);
+            return $serializer->serialize($bookList, 'json', $context);
+        });
+
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+
+    /**
+     * Cette méthode permet de récupérer un livre en particulier en fonction de son id. 
+     *
+     * @param Book $book
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getDetailBook(Book $book, SerializerInterface $serializer, VersioningService $versioningService): JsonResponse 
+    {
+        $version = $versioningService->getVersion();
+        $context = SerializationContext::create()->setGroups(["getBooks"]);
+        $context->setVersion($version);
+        $jsonBook = $serializer->serialize($book, 'json', $context);
+        return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+      
+    /**
+     * Cette méthode permet de supprimer un livre par rapport à son id. 
+     *
+     * @param Book $book
+     * @param EntityManagerInterface $em
+     * @return JsonResponse 
+     */
+    #[Route('/api/books/{id}', name: 'deleteBook', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour supprimer un livre')]
+    public function deleteBook(Book $book, EntityManagerInterface $em, TagAwareCacheInterface $cache): JsonResponse {
+        $em->remove($book);
+        $em->flush();
+        // On vide le cache.
+        $cache->invalidateTags(["booksCache"]);
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Cette méthode permet d'insérer un nouveau livre. 
+     * Exemple de données : 
+     * {
+     *     "title": "Le Seigneur des Anneaux",
+     *     "coverText": "C'est l'histoire d'un anneau unique", 
+     *     "idAuthor": 5
+     * }
+     * 
+     * Le paramètre idAuthor est géré "à la main", pour créer l'association
+     * entre un livre et un auteur. 
+     * S'il ne correspond pas à un auteur valide, alors le livre sera considéré comme sans auteur. 
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param AuthorRepository $authorRepository
+     * @return JsonResponse
+     */
+    #[Route('/api/books', name:"createBook", methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour créer un livre')]
+    public function createBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $em,
+        UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository, ValidatorInterface $validator,
+        TagAwareCacheInterface $cache): JsonResponse {
+
+        $book = $serializer->deserialize($request->getContent(), Book::class, 'json');
+
+        // On vérifie les erreurs
+        $errors = $validator->validate($book);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+            //throw new HttpException(JsonResponse::HTTP_BAD_REQUEST, "La requête est invalide");
+        }
+
+        $content = $request->toArray();
+        $idAuthor = $content['idAuthor'] ?? -1;
+        $book->setAuthor($authorRepository->find($idAuthor));
+
+        $em->persist($book);
+        $em->flush();
+
+        // On vide le cache. 
+        $cache->invalidateTags(["booksCache"]);
+
+        $context = SerializationContext::create()->setGroups(["getBooks"]);
+        $jsonBook = $serializer->serialize($book, 'json', $context);
+		
+        $location = $urlGenerator->generate('detailBook', ['id' => $book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+		return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);	
+    }
+    
+    
+    /**
+     * Cette méthode permet de mettre à jour un livre en fonction de son id. 
+     * 
+     * Exemple de données : 
+     * {
+     *     "title": "Le Seigneur des Anneaux",
+     *     "coverText": "C'est l'histoire d'un anneau unique", 
+     *     "idAuthor": 5
+     * }
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param Book $currentBook
+     * @param EntityManagerInterface $em
+     * @param AuthorRepository $authorRepository
+     * @return JsonResponse
+     */
+    #[Route('/api/books/{id}', name:"updateBook", methods:['PUT'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour éditer un livre')]
+    public function updateBook(Request $request, SerializerInterface $serializer,
+                        Book $currentBook, EntityManagerInterface $em, AuthorRepository $authorRepository, 
+                        ValidatorInterface $validator, TagAwareCacheInterface $cache): JsonResponse {
+       
+        $newBook = $serializer->deserialize($request->getContent(), Book::class, 'json');
+
+        $currentBook->setTitle($newBook->getTitle());
+        $currentBook->setCoverText($newBook->getCoverText());
+
+        // On vérifie les erreurs
+        $errors = $validator->validate($currentBook);
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $content = $request->toArray();
+        $idAuthor = $content['idAuthor'] ?? -1;
+
+        $currentBook->setAuthor($authorRepository->find($idAuthor));
+
+        $em->persist($currentBook);
+        $em->flush();
+        
+        // On vide le cache. 
+        $cache->invalidateTags(["booksCache"]);
+
+        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+    }
+
+
+}
